11fb1fb488a327d2164b013f56f69dcc9e2855fa
Gabriel Figueroa <gyfigueroa@ucdavis.edu>
2025-09-20 13:13:38 -0700

    path fixes

--- filters.js
+++ filters.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
   const svgFilters = `
     <svg style="display:none;">
       <filter id="displacementFilter">
-        <feImage href="/images/displacement.png" preserveAspectRatio="none" />
+        <feImage href="images/displacement.png" preserveAspectRatio="none" />
         <feDisplacementMap in="SourceGraphic" scale="300" xChannelSelector="R" yChannelSelector="G" />
       </filter>
       <filter id="displacementFilterCircular">
-        <feImage href="/images/displacement-circular.png" preserveAspectRatio="none" />
+        <feImage href="images/displacement-circular.png" preserveAspectRatio="none" />
         <feDisplacementMap in="SourceGraphic" scale="300" xChannelSelector="R" yChannelSelector="G" />
       </filter>
       <filter id="displacementFilterMenu">
-        <feImage href="/images/displacement-menu.png" preserveAspectRatio="none" />
+        <feImage href="images/displacement-menu.png" preserveAspectRatio="none" />
         <feDisplacementMap in="SourceGraphic" scale="300" xChannelSelector="R" yChannelSelector="G" />
       </filter>
     </svg>
