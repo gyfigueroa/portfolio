4c36112da2eb6dafb5b52db70922d0e8ad0febde
Gabriel Figueroa <gyfigueroa@ucdavis.edu>
2025-09-20 13:02:51 -0700

    fixed svg filters

---
+++ filters.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const svgFilters = `
+    <svg style="display:none;">
+      <filter id="displacementFilter">
+        <feImage href="/images/displacement.png" preserveAspectRatio="none" />
+        <feDisplacementMap in="SourceGraphic" scale="300" xChannelSelector="R" yChannelSelector="G" />
+      </filter>
+      <filter id="displacementFilterCircular">
+        <feImage href="/images/displacement-circular.png" preserveAspectRatio="none" />
+        <feDisplacementMap in="SourceGraphic" scale="300" xChannelSelector="R" yChannelSelector="G" />
+      </filter>
+      <filter id="displacementFilterMenu">
+        <feImage href="/images/displacement-menu.png" preserveAspectRatio="none" />
+        <feDisplacementMap in="SourceGraphic" scale="300" xChannelSelector="R" yChannelSelector="G" />
+      </filter>
+    </svg>
+  `;
+
+  document.body.insertAdjacentHTML("afterbegin", svgFilters);
+});
