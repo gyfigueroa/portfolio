6d38abaf0bcf2d29d2c8c26af99bd4a3ccdc46bb
Gabriel Figueroa <gyfigueroa@ucdavis.edu>
2025-09-23 11:26:01 -0700

    added loader

--- script.js
+++ script.js
@@ -4,6 +4,19 @@
 
     console.log('gsap running');
 
+
+    var loader = document.getElementById("preloader");
+    window.addEventListener("load", function () {
+        loader.style.display = "none";
+    });
+
+    //Fade out, optional
+    var s = document.getElementById("preloader").style;
+    s.opacity = 1;
+    (function fade() {
+        (s.opacity -= 0.1) < 0 ? (s.display = "none") : setTimeout(fade, 40);
+    })();
+
     let developers = document.querySelectorAll('.developer');
 
     //developers for home page
