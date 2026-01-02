a1eac0c8d54fa632a3de982b7cb90f284ebe0d50
Gabriel Figueroa <gyfigueroa@ucdavis.edu>
2026-01-01 16:32:16 -0800

    redirect overlays to vercel portfolio

--- script.js
+++ script.js
@@ -47,22 +47,7 @@
 
     setTimeout(animateEmojis, 500);
 
-    //console.log('script completed');
 
-    var loader = document.getElementById("preloader");
-    window.addEventListener("load", function () {
-        loader.style.display = "none";
-        //console.log('loaded screen, fading out loader')
-    });
-
-    //Fade out
-    var s = document.getElementById("preloader").style;
-    s.opacity = 1;
-    (function fade() {
-        (s.opacity -= 0.1) < 0 ? (s.display = "none"/* , console.log("faded out") */) : setTimeout(fade, 40);
-    })();
-
-    
 
 
 })();
